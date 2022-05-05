0eec1fe5369af0f8421fb09c0ccec0ff014c89f8
andrerodrigues <andrerodrigues29101992@gmail.com>
2022-05-05 15:10:16 +0100

    added colors and refatored code

--- src/components/AppButton/AppButton.jsx
+++ src/components/AppButton/AppButton.jsx
@@ -1,15 +1,17 @@
 import React from "react";
+import colors from "styles/_mixins.scss";
 import styles from "./appbutton.module.scss";
 
 const AppButton = (props) => {
+  const { color, label } = props;
   const styling = {
-    backgroundColor: props.color !== "green" && "rgba(255, 255, 255, 0.5)",
-    color: props.color !== "green" && "black",
-    border: props.color !== "green" && "1px solid #b0b0b0"
+    backgroundColor: !color ? "rgba(255, 255, 255, 0.5)" : colors[color],
+    color: !color ? colors.dark : "white",
+    border: !color && "1px solid #b0b0b0"
   };
   return (
     <button style={styling} className={styles.appButton} {...props}>
-      {props.label}
+      {label}
     </button>
   );
 };
